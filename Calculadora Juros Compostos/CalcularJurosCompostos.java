7cab273ca5ab3285224bae7c760f7cda30ac4538
Rodrigo Franchini Cecchin <rofranchinicecchin@gmail.com>
2024-09-06 18:02:50 -0300

    Delete Files
    Deletando os arquivos

--- Calculadora Juros Compostos/CalcularJurosCompostos.java
+++
@@ -1,7 +0,0 @@
-public class CalcularJurosCompostos {
-
-    // Método que calcula os juros compostos
-    public double calcular(double principal, double taxa, int periodos) {
-        return principal * Math.pow(1 + taxa, periodos); // Fórmula Juros Compostos
-    }
-}
